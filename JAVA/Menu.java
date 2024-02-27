5a23e46ed5e689df9abe46399a080eda03393665
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-02-27 16:03:06 -0300

    Prova 1 Java - Segundo Semestre 28/02/2024

---
+++ JAVA/Menu.java
@@ -0,0 +1,22 @@
+package projeto;
+
+public class Menu {
+    
+    public static void menu() {
+        System.out.println("Seja bem vindo! \n");
+        System.out.println("Selecione uma opção desejada: \n");
+        System.out.println("1 - Cadastrar novo produto");
+        System.out.println("2 - Consultar Lista de produtos");
+        System.out.println("3 - Consultar produto único");
+        System.out.println("4 - Alterar produto");
+        System.out.println("5 - Excluir produto");
+        System.out.println("6 - Abrir comanda");
+        System.out.println("7 - Consultar comanda");
+        System.out.println("8 - adicionar produto na comanda");
+        System.out.println("9 - Limpar comanda");
+        System.out.println("10 - Excluir comanda");
+        System.out.println("11 - Comanda - Pagamento");
+        System.out.println("12 - Realizar pagamento");
+        System.out.println("13 - Sair");
+    }
+}
